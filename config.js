d0ce39d13f0834449818ce72efc9f38e2a85088e
leroicarmel7-png <leroicarmel7@gmail.com>
2026-09-10 02:49:20 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
-
 export const botConfig = {
-  ownerName: "༼ 𝐑Ø𝐈༽ †🌹ᴼᴿᴵᴳᴵᴺᴬᴸ•🐦‍🔥𝐊𝐀𝐙𝐀𝐍",
-  ownerNumber: "22890123456" // << CHANGE ICI TON NUMÉRO,
+  ownerName: "/ROI\†🌹ORIGINAL•🐦‍🔥KAZAN",
+  ownerNumber: "22891847613", // << CHANGE ICI TON NUMÉRO
   ownerTag: "Lrxd Carmel",
   botName: "KAZAN MD",
   prefix: ".",
   version: "2.0 BETA",
   mode: "private",
   logo: "https://i.imgur.com/xyz.png",
-  initiatorTag: "༼ 𝐑Ø𝐈༽ †🌹ᴼᴿᴵᴳᴵᴺᴬᴸ•🐦‍🔥𝐊𝐀𝐙𝐀𝐍",
+  initiatorTag: "/ROI\†🌹ORIGINAL•🐦‍🔥KAZAN",
   messages: {
-    annihilation: `Purge by 𓍯⃝🐦‍🔥𝐊𝐀𝐙𝐀𝐍 domination\n\ndisparaissez`,
-    countPreview: `┌─『 𓍯⃝🐦‍🔥 COMPTAGE 』\n│\n│ ✦ Cibles détectées: {count}\n│ ✦ Initiateur: ༼ 𝐑Ø𝐈༽ †🌹ᴼᴿᴵᴳᴵᴺᴬᴸ•🐦‍🔥𝐊𝐀𝐙𝐀𝐍\n│\n└────────────────`,
-    notAdmin: "❌ Seulement ༼ 𝐑Ø𝐈༽ †🌹ᴼᴿᴵᴳᴵᴺᴬᴸ•🐦‍🔥𝐊𝐀𝐙𝐀𝐍 et les admins peuvent faire ça.",
+    annihilation: `Purge by 🔥🐦‍🔥KAZAN domination\n\ndisparaissez`,
+    countPreview: `╭─「 🔥🐦‍🔥 COMPTAGE 」\n|\n| ✦ Cibles détectées: {count}\n| ✦ Initiateur...`,
+    notAdmin: "❌ Seulement /ROI\†🌹ORIGINAL•🐦‍🔥KAZAN et les admins peuvent faire ça.",
     botNotAdmin: "❌ Je ne suis pas admin, mets-moi admin d'abord."
   }
 }
